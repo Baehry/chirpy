eb450f247924efa15a39e820a87e5066473263cb
Baehry <asaf2006b@gmail.com>
2025-12-24 19:06:30 +0200

    CH2: Routing; L2: Stateful Handlers

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"net/http"
 	"fmt"
 	"os"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", Handler)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", HealthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.MetricsHandler)
+	mux.HandleFunc("/reset", apiCfg.ResetHandler)
 	server := http.Server {
 		Handler: mux,
 		Addr: ":8080",
@@ -21,8 +29,27 @@ func main() {
 	}
 }
 
-func Handler(writer http.ResponseWriter, request *http.Request) {
+func HealthzHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) MetricsHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) ResetHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
