c74628568d4a2bd3b36069b228adeff75907ed14
Baehry <asaf2006b@gmail.com>
2026-01-11 11:20:10 +0200

    CH5: Storage; L5: Create User

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+import _ "github.com/lib/pq"
 import (
 	"net/http"
 	"fmt"
@@ -7,25 +8,43 @@ import (
 	"sync/atomic"
 	"encoding/json"
 	"strings"
+	"github.com/joho/godotenv"
+	"github.com/Baehry/chirpy/internal/database"
+	"database/sql"
+	"time"
+	"github.com/google/uuid"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries *database.Queries
+	platform string
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	dbQueries := database.New(db)
 	var apiCfg apiConfig
+	apiCfg.dbQueries = dbQueries
+	apiCfg.platform = os.Getenv("PLATFORM")
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", HealthzHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.MetricsHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.ResetHandler)
 	mux.HandleFunc("POST /api/validate_chirp", ValidateChirpHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.UsersHandler)
 	server := http.Server {
 		Handler: mux,
 		Addr: ":8080",
 	}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		os.Exit(1)
@@ -46,8 +65,13 @@ func (cfg *apiConfig) MetricsHandler(writer http.ResponseWriter, request *http.R
 
 func (cfg *apiConfig) ResetHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	if cfg.platform != "dev" {
+		writer.WriteHeader(403)
+		return
+	}
 	writer.WriteHeader(200)
 	cfg.fileserverHits.Store(0)
+	cfg.dbQueries.ResetUsers(request.Context())
 }
 
 func ValidateChirpHandler(writer http.ResponseWriter, request *http.Request) {
@@ -86,6 +110,34 @@ func ValidateChirpHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(200)
 	writer.Write(dat)
 	return
+}
+
+func (cfg *apiConfig) UsersHandler(writer http.ResponseWriter, request *http.Request) {
+	type parameters struct {
+        Email string `json:"email"`
+    }
+	type User struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+	decoder := json.NewDecoder(request.Body)
+    var params parameters
+    decoder.Decode(&params)
+	user, err := cfg.dbQueries.CreateUser(request.Context(), params.Email)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	actualUser := User{
+		ID: user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+	}
+	dat, _ := json.Marshal(actualUser)
+	writer.WriteHeader(201)
+	writer.Write(dat)
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
